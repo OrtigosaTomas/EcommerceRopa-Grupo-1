7649a1d3f1a94d895ee890a9ac6eb581f0e8c189
bruno-ponzano-davinci <89162398+bruno-ponzano-davinci@users.noreply.github.com>
2023-05-30 18:38:03 -0300

    bruno

---
+++ Grupo1/Grupo1/Form3.cs
@@ -0,0 +1,125 @@
+﻿using Grupo1.Properties;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grupo1
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+
+        public static class GlobalVariables
+        {
+            public static string Usuario { get; set; }
+        }
+
+
+        private bool login(string username, string password)
+        {
+            try
+            {
+                string server = "localhost";
+                string database = "mydb";
+                string user = "root";
+                string pass = "";
+                string cadenaConexion = "server=" + server + ";database=" + database + ";" + "Uid=" + user + ";" + ";pwd=" + pass + ";";
+
+                MySqlConnection myCon = new MySqlConnection(cadenaConexion);
+                myCon.Open();
+
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM cliente WHERE usuario=@username AND contraseña=@password", myCon);
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@password", password);
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                return reader.HasRows;
+                
+            }
+            catch (Exception error)
+            {
+                 Console.WriteLine("Error de conexion " + error);
+                return false;
+            }
+        }
+
+     
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        private void button2_Click(object sender, EventArgs e)
+        {
+            {
+                if (isValid())
+                {
+                    string username = textBox1.Text.Trim();
+                    string password = textBox2.Text.Trim();
+
+
+
+
+                    
+                    if (login(username, password))
+                    {
+                        MessageBox.Show("Inicio de sesión exitoso", "Éxito");
+                        Hide();
+                        Home home = new Home();  
+                        home.Show();
+
+                    
+
+                        GlobalVariables.Usuario = textBox1.Text;
+                        
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Nombre de usuario o contraseña incorrectos", "Error");
+                    }
+                }
+            }
+        }
+
+        private void uername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool isValid()
+        {
+            if(textBox1.Text.TrimStart()== string.Empty)
+            {
+
+                MessageBox.Show("Ingresa un usuario","error");
+                return false;
+            }else if (textBox2.Text.TrimStart()== string.Empty)
+            {
+
+                MessageBox.Show("Ingresa una contraseña","error");
+                return false;
+            }
+            return true;
+        }
+
+        internal void SetUsername(object username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    }
+
---
+++ Grupo1/Grupo1/Form5.cs
@@ -0,0 +1,236 @@
+﻿using Grupo1.Controlador;
+using Grupo1.Modelos;
+using Grupo1.Properties;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Grupo1
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+           
+        }
+
+
+        Gorra_Controlador gorController = new Gorra_Controlador();
+        int DEBUG_MODE = 1;
+
+
+        private void Form5_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click1(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void boton_crear_Click_1(object sender, EventArgs e)
+        {
+            Trace.WriteLine("asd " );
+            try
+            {
+                string server = "localhost";
+                string database = "mydb";
+                string user = "root";
+                string pass = "";
+                string cadenaConexion = "server=" + server + ";database=" + database + ";" + "Uid=" + user + ";" + ";pwd=" + pass + ";";
+
+                MySqlConnection myCon = new MySqlConnection(cadenaConexion);
+                myCon.Open();
+
+                MySqlCommand cmd = new MySqlCommand("SELECT MAX(Id) FROM indumentaria", myCon);
+                int lastId = Convert.ToInt32(cmd.ExecuteScalar());
+                Trace.WriteLine("asd " + lastId);
+
+                Gorra gor_nueva = new Gorra();
+
+                gor_nueva.Id = lastId + 1;
+
+                myCon.Close();
+
+
+                Trace.WriteLine("4");
+
+
+                gor_nueva.Nombre = txt_nombre1.Text;
+
+                gor_nueva.Tipo = txt_tipo1.Text;
+
+                gor_nueva.Detalle = txt_detalle1.Text;
+
+                gor_nueva.Genero_id = 3;
+
+                gor_nueva.Precio = int.Parse(txt_precio1.Text);
+
+                Trace.WriteLine("3");
+
+
+                if (talleBox2.SelectedItem.ToString() == "10")
+                {
+                    gor_nueva.Talle_id = 1;
+                }
+                else if (talleBox2.SelectedItem.ToString() == "20")
+                {
+
+                    gor_nueva.Talle_id = 2;
+                }
+                else if (talleBox2.SelectedItem.ToString() == "30")
+                {
+
+                    gor_nueva.Talle_id = 3;
+                }
+                else if (talleBox2.SelectedItem.ToString() == "40")
+                {
+
+                    gor_nueva.Talle_id = 4;
+                }
+                else if (talleBox2.SelectedItem.ToString() == "50")
+                {
+
+                    gor_nueva.Talle_id = 5;
+                }
+                else if (talleBox2.SelectedItem.ToString() == "60")
+                {
+
+                    gor_nueva.Talle_id = 6;
+                }
+
+
+
+
+                Trace.WriteLine("2");
+
+
+                if (categoriaBox1.SelectedItem.ToString() == "Piluso")
+                {
+                    gor_nueva.Categoria_id = 3;
+                }
+                else if (categoriaBox1.SelectedItem.ToString() == "Plana")
+                {
+
+                    gor_nueva.Categoria_id = 5;
+                }
+                else if (categoriaBox1.SelectedItem.ToString() == "Curva")
+                {
+
+                    gor_nueva.Categoria_id = 6;
+                }
+
+
+
+                gor_nueva.Stock_id = int.Parse(txt_stockid1.Text);
+
+
+                Trace.WriteLine("1");
+
+
+
+                gorController.crearGorra(gor_nueva);
+
+
+                Trace.WriteLine("44");
+
+
+
+                if (DEBUG_MODE == 1)
+                {
+
+                    Trace.WriteLine("Se esta creando una zapatilla");
+                }
+                if (DEBUG_MODE == 2)
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine("Ocurrio un error al crear una nueva zapatilla" + ex);
+            }
+        }
+
+        private void btn_form21(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void boton_form2_Click1(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            DialogResult dialogResult = form2.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                Trace.WriteLine("OK");
+            }
+
+
+            this.Hide();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form4 form4 = new Form4();
+            DialogResult dialogResult = form4.ShowDialog();
+            this.Hide();
+        }
+
+        private void txt_nombre_TextChanged1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void generoBox_SelectedIndexChanged1(object sender, EventArgs e)
+        {
+
+
+        }
+
+    
+
+        private void txt_id1_TextChanged1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void categoriaBox1_SelectedIndexChanged1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void talleBox2_SelectedIndexChanged1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Form4 form4 = new Form4();
+            DialogResult dialogResult = form4.ShowDialog();
+            
+        }
+
+        private void generoBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
